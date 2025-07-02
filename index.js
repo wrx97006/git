f782fb565b76d5b2f60bb820d538907168fc3c57
wrx97006 <wrx97006@student.wroclaw.merito.pl>
2025-07-02 23:56:44 +0200

    feat: initial Node.js server with health endpoint

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello Node.js!');
+    } else if (req.url === '/health') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('OK');
+    } else {
+        res.writeHead(404);
+        res.end('Not Found');
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
